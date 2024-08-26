059929e9fd1d5a763289cf69ff7bf2095e694d22
xerootg <xerootg@users.noreply.github.com>
2024-08-25 21:38:36 -0600

    Change default logcat filter

--- cryze_android_app/fork_logger/dockercat.c
+++ cryze_android_app/fork_logger/dockercat.c
@@ -9,7 +9,13 @@ int main() {
 
     // Check if our custom descriptor exists on /init
     if (stat(stdout_descriptor, &st) == 0) {
-        execl("/system/bin/logcat", "logcat", "-f", stdout_descriptor, "-e", "com.tencentcs.iotvideo", (char *)NULL);
+        execl("/system/bin/logcat", "logcat", "-f", stdout_descriptor, "-b", "main",
+          "hwcomposer:s", "TcpSocketTracker:s", "cmd:s", "Netd:s", "TcpSocketMonitor:s", "chatty:s",
+          "KeyguardClockSwitch:s", "system_server:s", "dex2oat32:s", "installd:s", "BackgroundDexOptService:s",
+          "UserPackageInfos:s", "PermissionControllerServiceImpl:s", "LancherPackagesLiveData:s", "PackageDexOptimizer:s",
+          "LatinIME:s", "AvrcpMediaPlayerList:s", "AvrcpBrowsablePlayerConnector:s", "UserSensitiveFlagsUtils:s",
+          "PhoneInterfaceManager:s", "CarrierSvcBindHelper:s", (char *)NULL);
+        //execl("/system/bin/logcat", "logcat", "-f", stdout_descriptor, "-e", "com.tencentcs.iotvideo", (char *)NULL);
     } else {
         printf("no docker log descriptor found at %s\n", stdout_descriptor);
         return 0;
